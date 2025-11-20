5f97e8c9574ddf0e4998cae981fddb81010bdb01
srivasthav-2036 <srivasthav599@outlook.com>
2025-11-20 19:48:52 +0530

    req fix

--- ai_modules/pdf_processor.py
+++ ai_modules/pdf_processor.py
@@ -3,8 +3,7 @@ from PyPDF2 import PdfReader
 import pdfplumber
 import fitz  # PyMuPDF
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-import google.generativeai as genai
+from langchain_huggingface import HuggingFaceEmbeddingsimport google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
